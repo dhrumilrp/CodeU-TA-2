a4bb343a65fc1e8dcfeff5188f508815f7555b80
dhrumilrp <dhrumil.r.patel95@gmail.com>
2015-04-03 12:26:31 -0400

    completed FilterList andFlatTree

--- FlatTree.java
+++ FlatTree.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 
 public class FlatTree {
-		
+	
+	static ArrayList<String> tree = new ArrayList<String>();
+	
 	static class BinaryTree{
 		BinaryTree left;
 		BinaryTree right;
@@ -12,10 +16,35 @@ public class FlatTree {
 			s = payload;
 		}
 	}
+	
+	static void flatten (BinaryTree root){
+		if(root == null) return;
+		if(root.left != null )  flatten(root.left);
+		tree.add(root.s);
+		if(root.right != null )  flatten(root.right);
+	}
 
 	public static void main(String[] args){
-		BinaryTree node = new BinaryTree( new BinaryTree(null, null, "hi")
-				, null, "hello");
-		System.out.println(node.left);
+		BinaryTree node0 = new BinaryTree(null, null, "Google");
+		BinaryTree node1 = new BinaryTree(null, null, "CodeU");
+		BinaryTree node2 = new BinaryTree(null, null, "is");
+		BinaryTree node3 = new BinaryTree(null, null, "awesome");
+		BinaryTree node4 = new BinaryTree(null, null, "woot woot");
+		BinaryTree node5 = new BinaryTree(null, null, "summertime");
+		BinaryTree node6 = new BinaryTree(null, null, "Dhrumil");
+		
+		node0.left = node1;
+		node0.right = node2;
+		node1.left = node3;
+		node1.right = node4;
+		node2.left = node5;
+		node2.right = node6;
+		
+		flatten(node0);
+		
+		for(String a : tree){
+			System.out.println(a);
+		}
+
 	}
 }
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # CodeU-TA-2
 Google CodeU Technical Assignment #2
 
-This is the second technical exercise set from Google CodeU
+This is the second technical exercise set from Google CodeU.
+The exercises cover Lists, Trees, and Graphs. 
